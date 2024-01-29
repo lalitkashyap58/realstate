521beef0b4663151aff732cdbc64302da84e7d81
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2024-01-29 12:39:56 +0530

    pages and routes set

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
-
-function App() {
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
   
-
+const App=()=>{
   return (
-    <>
-      <h1>Real State</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>} />  <Route path="/about" element={<About/>} />  <Route path="/signin" element={<Signin/>} />
+      <Route path="/signup" element={<SignUp/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
+  
+
 
 export default App
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+   <>
+   Signup
+   </>
+  )
+}
+
+export default SignUp;
